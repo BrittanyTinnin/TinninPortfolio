de414c8097b701fc347527556f2034af79bbfbfb
github username <bntinnin7@gmail.com>
2019-05-10 01:44:59 -0400

    Added bootstrap via ruby gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_202721) do
+ActiveRecord::Schema.define(version: 2019_05_09_220508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_05_09_202721) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status"
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
@@ -50,6 +51,12 @@ ActiveRecord::Schema.define(version: 2019_05_09_202721) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
