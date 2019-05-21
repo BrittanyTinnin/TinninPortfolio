71659e954cb359a31b053d434e9d27c2b99c4404
github username <bntinnin7@gmail.com>
2019-05-20 23:57:17 -0400

    Added seed data for Topic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+1.times do
+	Topic.create!(
+		title: "JavaScript"
+	)
+end
 
 12.times do 
     Blog.create!(
