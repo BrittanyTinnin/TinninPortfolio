a8b5f2bc56f3ca02342df610616f347b50e15c3f
github username <bntinnin7@gmail.com>
2019-05-30 17:47:16 -0400

    Remove aws-cli file and add friendly slugs for Portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,8 @@ class Portfolio < ApplicationRecord
   mount_uploader :thumb_image, ImageUploader
   mount_uploader :main_image, ImageUploader
 
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 
   validates :title, :body, presence: true
 end
