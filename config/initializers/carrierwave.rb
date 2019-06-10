6251cba203171dd52636927d1d5b1226d86a4398
github username <bntinnin7@gmail.com>
2019-06-10 16:02:20 -0400

    updated aws region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required unless using use_iam_profile
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
-    region:                ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
+    region:                'us-east-2',                  # optional, defaults to 'us-east-1'
     :path_style            => true
   }
   config.fog_directory  = 'tinnin-brittany-two-portfolio'                                      # required
