7e5a55edfb7b2233b506632f8252a7f7b561f5d5
github username <bntinnin7@gmail.com>
2019-06-11 19:45:53 -0400

    commented out JS code

--- app/assets/javascripts/portfolios.js
+++ app/assets/javascripts/portfolios.js
@@ -2,13 +2,13 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(
-  $('.custom-file-input').on('change', function() { 
-    let fileName = $(this).val().split('\\').pop();
-    console.log(fileName) 
-    $(this).next('.custom-file-label').addClass("selected").html(fileName); 
-  })
-)
+// $(document).ready(
+//   $('.custom-file-input').on('change', function() { 
+//     let fileName = $(this).val().split('\\').pop();
+//     console.log(fileName) 
+//     $(this).next('.custom-file-label').addClass("selected").html(fileName); 
+//   })
+// )
 
 // let portfolioPage = document.getElementsByClassName('custom-file')
 // portfolioPage.ready(
