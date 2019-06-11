35b9836da45da954b7401f2afe65d002cfdaa25e
github username <bntinnin7@gmail.com>
2019-06-11 19:27:46 -0400

    Add JS for file upload

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,9 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// let welcome = document.getElementsByClassName('welcome')
+
+// welcome.ready(
+//   alert("Welcome to Home Page, JavaScript style!")
+// )
--- app/assets/javascripts/portfolios.js
+++ app/assets/javascripts/portfolios.js
@@ -2,10 +2,18 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-$('.custom-file-input').on('change', function() { 
-  let fileName = $(this).val().split('\\').pop(); 
-  $(this).next('.custom-file-label').addClass("selected").html(fileName); 
-});
+$(document).ready(
+  $('.custom-file-input').on('change', function() { 
+    let fileName = $(this).val().split('\\').pop();
+    console.log(fileName) 
+    $(this).next('.custom-file-label').addClass("selected").html(fileName); 
+  })
+)
+
+// let portfolioPage = document.getElementsByClassName('custom-file')
+// portfolioPage.ready(
+//   alert("Welcome to Portfolio Form!")
+// )
 
 // $(document).ready ->
 
